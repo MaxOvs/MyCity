756635386fb9c3473777cded54d95db1f4d36339
David Rimbault <rimbault.david@gmail.com>
2014-06-09 23:01:15 +0200

    LocationAdmin

---
+++ src/JyAccede/Bundle/LocationBundle/Admin/LocationAdmin.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: david
+ * Date: 09/06/2014
+ * Time: 22:52
+ */
+
+namespace JyAccede\Bundle\ActuBundle\Admin;
+
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class LocationAdmin extends Admin
+{
+   // Fields to be shown on create/edit forms
+   protected function configureFormFields(FormMapper $formMapper)
+   {
+       $formMapper
+           ->add('title', 'text', array('label' => 'Post Title'))
+       ;
+   }
+
+   // Fields to be shown on filter forms
+   protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+   {
+       $datagridMapper
+           ->add('title')
+       ;
+   }
+
+   // Fields to be shown on lists
+   protected function configureListFields(ListMapper $listMapper)
+   {
+       $listMapper
+           ->addIdentifier('title')
+       ;
+   }
+}
